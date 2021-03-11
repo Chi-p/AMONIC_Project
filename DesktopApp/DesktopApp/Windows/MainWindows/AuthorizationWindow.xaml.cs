249fb32e6d5b3b6046e74788604f7ddd658c7976
Chi-p <silviroshcnv@gmail.com>
2021-03-12 02:35:59 +0300

    add new styles, create authorization window

---
+++ DesktopApp/DesktopApp/Windows/MainWindows/AuthorizationWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopApp.Windows.MainWindows
+{
+    /// <summary>
+    /// Interaction logic for AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
