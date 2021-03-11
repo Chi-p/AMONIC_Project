d6f26bc711431583d126fd3973f2b9fc5834cece
Chi-p <silviroshcnv@gmail.com>
2021-03-11 14:59:41 +0300

    add entity and AppData.cs

---
+++ DesktopApp/DesktopApp/Entities/AppData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DesktopApp.Entities
+{
+    public class AppData
+    {
+        public static AMONICEntities Context = new AMONICEntities();
+        public static Frame MainFrame;
+        public static Users CurrentUser;
+    }
+}
